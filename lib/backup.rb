f2fa201c1c2c4c9d64bd50105713cf9afe001d34
Daniel Andrew <dwandrew@hotmail.com>
2020-04-03 13:22:58 +0000

    Done.

---
+++ lib/backup.rb
@@ -0,0 +1,14 @@
+
+#def square_array(array)
+#  i=0;
+ # squared_array = []
+#  while i< array.length do
+#  squared_array.push(array[i]*array[i])
+#  i+=1
+#  end
+#  return squared_array
+#  while i< array.length do
+#  square_array.push(array[i]*array[i])
+#  i+=1
+#  end
+#end
--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,8 @@
-
 def square_array(array)
-  i=0;
-  squared_array = []
-  while i< array.length do
-  squared_array.push(array[i]*array[i])
-  i+=1
-  end
-  return squared_array
+  square_array=[]
+  array.length.times{
+    |index|
+    square_array.push(array[index]**2)
+  }
+  square_array
 end
